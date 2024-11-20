18ef361e57c6b6ac0b32df49b76c6a3015fcd76c
xuedinge <781408517@qq.com>
2024-11-21 01:52:36 +0800

    feat:LoginResponse业务和系统分开

--- server/model/system/response/sys_user.go
+++ server/model/system/response/sys_user.go
@@ -9,27 +9,15 @@ type SysUserResponse struct {
 }
 
 type LoginResponse struct {
-	User      system.SysUser `json:"user"`
-	Token     string         `json:"token"`
-	ExpiresAt int64          `json:"expiresAt"`
+	Token     string `json:"token"`
+	ExpiresAt int64  `json:"expiresAt"`
 }
 type LoginBussinessResponse struct {
-	User      system.BussinesUser `json:"user"`
-	Token     string              `json:"token"`
-	ExpiresAt int64               `json:"expiresAt"`
+	User          system.BussinesUser `json:"user"`
+	LoginResponse `json:",inline"`
 }
 
-// TODO 业务和系统分开
-//type LoginResponse struct {
-//	Token     string `json:"token"`
-//	ExpiresAt int64  `json:"expiresAt"`
-//}
-//type LoginBussinessResponse struct {
-//	User system.BussinesUser `json:"user"`
-//	LoginResponse
-//}
-//
-//type LoginSysUserResponse struct {
-//	User system.SysUser `json:"user"`
-//	LoginResponse
-//}
+type LoginSysUserResponse struct {
+	User          system.SysUser `json:"user"`
+	LoginResponse `json:",inline"`
+}
