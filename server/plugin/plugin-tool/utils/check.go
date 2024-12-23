097989887dd38460d0dbf3c2d1e1048b00ca20f0
xuedinge <781408517@qq.com>
2024-12-23 19:30:53 +0800

    other:修复git记录bug

--- server/plugin/plugin-tool/utils/check.go
+++ server/plugin/plugin-tool/utils/check.go
@@ -1,50 +1,89 @@
 package utils
 
 import (
-	"fmt"
+	"errors"
+
+	"gorm.io/gorm"
 
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
 	"github.com/flipped-aurora/gin-vue-admin/server/model/system"
 )
 
-func RegisterApis(apis ...system.SysApi) {
-	var count int64
-	var apiPaths []string
-	for i := range apis {
-		apiPaths = append(apiPaths, apis[i].Path)
+func RegisterApis(apis []system.SysApi, checkEveryOne ...bool) {
+	shouldCheckEveryOne := false
+	if len(checkEveryOne) > 0 {
+		shouldCheckEveryOne = checkEveryOne[0]
 	}
-	global.GVA_DB.Find(&[]system.SysApi{}, "path in (?)", apiPaths).Count(&count)
-	if count > 0 {
-		return
-	}
-	err := global.GVA_DB.Create(&apis).Error
-	if err != nil {
-		fmt.Println(err)
+
+	if shouldCheckEveryOne {
+		// 对每个 API 单独处理
+		for i := range apis {
+			var count int64
+			global.GVA_DB.Model(&system.SysApi{}).Where("path = ?", apis[i].Path).Count(&count)
+
+			if count == 0 {
+				// 如果不存在，则创建新的
+				if err := global.GVA_DB.Create(&apis[i]).Error; err != nil {
+					return
+				}
+			} else {
+				// 如果存在，可以选择更新或跳过，这里选择跳过
+				continue
+			}
+		}
+	} else {
+		// 检查是否有任何一个 API 的 Path 已存在
+		var count int64
+		var apiPaths []string
+		for _, api := range apis {
+			apiPaths = append(apiPaths, api.Path)
+		}
+		global.GVA_DB.Model(&system.SysApi{}).Where("path IN (?)", apiPaths).Count(&count)
+		if count > 0 {
+			// 如果有任何一个存在，直接返回
+			return
+		}
+		// 如果都不存在，批量创建
+		if err := global.GVA_DB.Create(&apis).Error; err != nil {
+			return
+		}
 	}
 }
 
-func RegisterMenus(menus ...system.SysBaseMenu) {
-	var count int64
-	var menuNames []string
+func RegisterMenus(menus []system.SysBaseMenu, isMount ...bool) {
+	// 处理默认值
+	shouldMount := false
+	if len(isMount) > 0 {
+		shouldMount = isMount[0]
+	}
+
 	parentMenu := menus[0]
 	otherMenus := menus[1:]
-	for i := range menus {
-		menuNames = append(menuNames, menus[i].Name)
-	}
-	global.GVA_DB.Find(&[]system.SysBaseMenu{}, "name in (?)", menuNames).Count(&count)
-	if count > 0 {
+
+	// 检查父菜单是否存在
+	err := global.GVA_DB.Where("name = ?", parentMenu.Name).First(&parentMenu).Error
+	if err != nil && errors.Is(err, gorm.ErrRecordNotFound) {
+		// 父菜单不存在，创建它
+		if err := global.GVA_DB.Create(&parentMenu).Error; err != nil {
+			global.GVA_LOG.Sugar().Error("注册父菜单%s失败", parentMenu.Name, err.Error())
+			return
+		}
+	} else if !shouldMount {
+		// 父菜单存在且不需要强制挂载，直接返回
 		return
 	}
-	err := global.GVA_DB.Create(&parentMenu).Error
-	if err != nil {
-		fmt.Println(err)
-	}
+
+	// 检查每个子菜单
 	for i := range otherMenus {
-		pid := parentMenu.ID
-		otherMenus[i].ParentId = pid
-	}
-	err = global.GVA_DB.Create(&otherMenus).Error
-	if err != nil {
-		fmt.Println(err)
+		var existCount int64
+		global.GVA_DB.Model(&system.SysBaseMenu{}).Where("name = ? AND parent_id = ?", otherMenus[i].Name, parentMenu.ID).Count(&existCount)
+		if existCount == 0 {
+			// 子菜单不存在，设置父 ID 并创建
+			otherMenus[i].ParentId = parentMenu.ID
+			if err := global.GVA_DB.Create(&otherMenus[i]).Error; err != nil {
+				global.GVA_LOG.Sugar().Error("注册子菜单%s失败", otherMenus[i].Name, err.Error())
+				return
+			}
+		}
 	}
 }
