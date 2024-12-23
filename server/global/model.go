40de9ac3ba553b444baf6bd7fde1e1362ec1e7a1
xuedinge <781408517@qq.com>
2024-12-23 16:10:03 +0800

    server:原生,GVA_MODEL绑定方法findById单个和多个查询

--- server/global/model.go
+++ server/global/model.go
@@ -12,3 +12,11 @@ type GVA_MODEL struct {
 	UpdatedAt time.Time      // 更新时间
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"` // 删除时间
 }
+
+func (self *GVA_MODEL) FindById(db *gorm.DB, out interface{}) error {
+	return db.First(out, self.ID).Error
+}
+
+func (self *GVA_MODEL) FindListById(db *gorm.DB, out interface{}) error {
+	return db.Find(out, self.ID).Error
+}
