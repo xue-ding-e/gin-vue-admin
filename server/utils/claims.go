570ae8c97bcbe8974f27268ff53ab858ab3fcf8c
xuedinge <781408517@qq.com>
2025-03-04 12:16:54 +0800

    feat:GetClaims GetToken日志改为Debug级别,根据需要自行调整Error级别

--- server/utils/claims.go
+++ server/utils/claims.go
@@ -44,7 +44,7 @@ func GetToken(c *gin.Context) string {
 		token = c.Request.Header.Get("x-token")
 		claims, err := j.ParseToken(token)
 		if err != nil {
-			global.GVA_LOG.Error("重新写入cookie token失败,未能成功解析token,请检查请求头是否存在x-token且claims是否为规定结构")
+			global.GVA_LOG.Debug("重新写入cookie token失败,未能成功解析token,请检查请求头是否存在x-token且claims是否为规定结构")
 			return token
 		}
 		SetToken(c, token, int((claims.ExpiresAt.Unix()-time.Now().Unix())/60))
@@ -57,7 +57,7 @@ func GetClaims(c *gin.Context) (*systemReq.CustomClaims, error) {
 	j := NewJWT()
 	claims, err := j.ParseToken(token)
 	if err != nil {
-		global.GVA_LOG.Error("从Gin的Context中获取从jwt解析信息失败, 请检查请求头是否存在x-token且claims是否为规定结构")
+		global.GVA_LOG.Debug("从Gin的Context中获取从jwt解析信息失败, 请检查请求头是否存在x-token且claims是否为规定结构")
 	}
 	return claims, err
 }
