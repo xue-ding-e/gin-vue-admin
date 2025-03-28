2e1d0e528d1caccc02b59e18187496d754b4e84a
xuedinge <781408517@qq.com>
2025-03-29 00:05:22 +0800

    other:deploy.js

--- web/deploy-template.js
+++ web/deploy-template.js
@@ -1,10 +1,12 @@
-const Client = require('ssh2-sftp-client');
-const path = require('path');
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import Client from 'ssh2-sftp-client'
 
-const sftp = new Client();
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const sftp = new Client()
 
-const localDir = path.resolve(__dirname, './dist/');
-const remoteDir = '服务器存放前端文件路径';
+const localDir = path.resolve(__dirname, './dist/')
+const remoteDir = '服务器存放前端文件路径'
 
 sftp.connect({
     host: '服务器ip',
@@ -13,15 +15,29 @@ sftp.connect({
     password: '服务器密码', // 或者使用 privateKey: fs.readFileSync('/path/to/key')
 })
     .then(() => {
-        console.log('连接成功，开始上传文件...');
-        return sftp.uploadDir(localDir, remoteDir);
+        console.log('连接成功，检测远程目录是否存在...')
+        return sftp.exists(remoteDir)
+    })
+    .then((exists) => {
+        if (exists) {
+            console.log('远程目录存在，开始删除...')
+            // 递归删除远程目录
+            return sftp.rmdir(remoteDir, true)
+        }
+        else {
+            console.log('远程目录不存在，跳过删除步骤...')
+        }
     })
     .then(() => {
-        console.log('上传成功！');
+        console.log('开始上传文件...')
+        return sftp.uploadDir(localDir, remoteDir)
     })
-    .catch(err => {
-        console.error('上传失败：', err);
+    .then(() => {
+        console.log('上传成功！')
+    })
+    .catch((err) => {
+        console.error('操作失败：', err)
     })
     .finally(() => {
-        sftp.end();
-    });
+        sftp.end()
+    })
